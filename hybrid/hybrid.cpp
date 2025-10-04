93dcc4e62a88156a062cbd97fd069d052e0d8d49
seb.nor.san <sejeseb99@gmail.com>
2025-10-04 13:18:30 +0200

    Made a hybrid example

---
+++ hybrid/hybrid.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+// Hybrid approach
+// This is a combination of the pIMPL idiom and the Abstract Interface
+// It separates concerns between the two models: 
+//   pIMPL is responsible for clearing out VTable fragility
+//   Abstract Interface is for SOLID compliance and development of model.
+
+// Public-facing wrapper (no vtables exposed!)
+// This is what is exposed through library API
+// WorkPimpl.h
+#include <memory>
+struct IWork; // no exposure of internals - opaque
+
+// A simple bridge, with a dependency injected opaque IWork
+class WorkPimpl {
+public:
+    WorkPimpl(std::unique_ptr<IWork> pimpl);
+    void DoWork();
+private:
+    std::unique_ptr<IWork> pimpl_; // points to abstract interface
+};
+
+// IWork.h
+// Public stable interface 
+struct IWork {
+    virtual void DoWork() = 0;
+    virtual ~IWork() = default;
+};
+
+// MyWork.h
+// Factory returns abstract implementations
+// include IWork.h
+std::unique_ptr<IWork> CreateMyWork();
+
+// MyWork.cpp
+
+struct MyWork : IWork {
+    void DoWork() override { 
+      std::cout << "Do something" << std::endl;
+    }
+};
+
+std::unique_ptr<IWork> CreateMyWork()
+{
+   return std::make_unique<MyWork>();
+}
+
+
+// WorkPimple.cpp
+// include IWork.h
+
+WorkPimpl::WorkPimpl(std::unique_ptr<IWork> pimpl) : pimpl_(std::move(pimpl)) 
+{}
+
+void WorkPimpl::DoWork() { pimpl_->DoWork(); } // non-virtual bridge
+
+
+int main()
+{
+   WorkPimpl work(CreateMyWork());
+   work.DoWork();
+}
